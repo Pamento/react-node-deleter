bd9287928933d4f09eca2bfd386941eb8084b991
Pamento <pamento.dev@gmail.com>
2019-04-15 15:39:05 +0200

    Update deleteFiles.js

--- app/services/deleteFiles.js
+++ app/services/deleteFiles.js
@@ -2,7 +2,6 @@ const del = require('del');
 
 
 deleteFile = async (filename) => {
-    // const deletedPaths = await del(['tmp/*.js', '!tmp/unicorn.js']);
     const deletedPaths = await del([`public/loads/${filename}.*`, '!public/loads']);
 
     console.log('Deleted files :\n', deletedPaths.join('\n'));
